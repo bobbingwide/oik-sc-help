e2aa2087e66fec4266e4e3e7d1ab9e316d7fdd6c
Herb Miller <herb@bobbingwide.com>
2016-04-02 18:29:52 +0100

    Add file for Genesis theme framework shortcodes

---
+++ shortcodes/genesis.php
@@ -0,0 +1,32 @@
+<?php // (C) Copyright Bobbing Wide 2015
+
+/**
+ * Shortcode help for Genesis shortcodes
+ *
+ * This link doesn't cover all the shortcodes
+ *
+ * {link https://my.studiopress.com/docs/shortcode-reference/}
+ 
+ 
+ footer.php:add_shortcode( 'footer_backtotop', 'genesis_footer_backtotop_shortcode' );
+ footer.php:add_shortcode( 'footer_copyright', 'genesis_footer_copyright_shortcode' );
+ footer.php:add_shortcode( 'footer_childtheme_link', 'genesis_footer_childtheme_link_shortcode' );
+ footer.php:add_shortcode( 'footer_genesis_link', 'genesis_footer_genesis_link_shortcode' );
+ footer.php:add_shortcode( 'footer_studiopress_link', 'genesis_footer_studiopress_link_shortcode' );
+ footer.php:add_shortcode( 'footer_wordpress_link', 'genesis_footer_wordpress_link_shortcode' );
+ footer.php:add_shortcode( 'footer_loginout', 'genesis_footer_loginout_shortcode' );
+ post.php:add_shortcode( 'post_date', 'genesis_post_date_shortcode' );
+ post.php:add_shortcode( 'post_time', 'genesis_post_time_shortcode' );
+ post.php:add_shortcode( 'post_modified_date', 'genesis_post_modified_date_shortcode' );
+ post.php:add_shortcode( 'post_modified_time', 'genesis_post_modified_time_shortcode' );
+ post.php:add_shortcode( 'post_author', 'genesis_post_author_shortcode' );
+ post.php:add_shortcode( 'post_author_link', 'genesis_post_author_link_shortcode' );
+ post.php:add_shortcode( 'post_author_posts_link', 'genesis_post_author_posts_link_shortcode' );
+ post.php:add_shortcode( 'post_comments', 'genesis_post_comments_shortcode' );
+ post.php:add_shortcode( 'post_tags', 'genesis_post_tags_shortcode' );
+ post.php:add_shortcode( 'post_categories', 'genesis_post_categories_shortcode' );
+ post.php:add_shortcode( 'post_terms', 'genesis_post_terms_shortcode' );
+ post.php:add_shortcode( 'post_edit', 'genesis_post_edit_shortcode' );
+
+ 
+ */
